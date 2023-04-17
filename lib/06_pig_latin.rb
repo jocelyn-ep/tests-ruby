06ce376a19a6916876bf397d85cddf8eb882af57
jocelyn-ep <jocelyn.epaillard.pro@gmail.com>
2023-04-17 18:12:55 +0200

    optimisation 06

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -7,28 +7,28 @@ def translate(big_string)
 
   array = big_string.split
 
-  array.each_with_index do |str, index| 
+  array.map! do |str| 
     if vowels.include?(str[0])
       str = str + 'ay'
-      array[index]=str
+      
     elsif sch.include?(str[0..2])
       str = str[3..-1] + str[0..2] + 'ay'
-      array[index]=str
+      
     elsif qu.include?(str[0..1])
       str = str[2..-1] + str[0..1] + 'ay'
-      array[index]=str
+      
     elsif consonants.include?(str[0]) && qu.include?(str[1..2]) 
       str = str[3..-1] + str[0..2] + 'ay'
-      array[index]=str
+      
     elsif consonants.include?(str[0]) && consonants.include?(str[1]) && consonants.include?(str[2])
       str = str[3..-1] + str[0..2] + 'ay'
-      array[index]=str
+      
     elsif consonants.include?(str[0]) && consonants.include?(str[1])
       str = str[2..-1] + str[0..1] + 'ay'
-      array[index]=str
+      
     elsif consonants.include?(str[0])
       str = str[1..-1] + str[0] + 'ay'
-      array[index]=str
+      
     end
   end
   
